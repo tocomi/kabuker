533b42d27d3794daf587fa23333d56720c5b5c36
tocomi <tocomi@wish.ocn.ne.jp>
2020-04-05 16:51:30 +0900

    chore: use firebase as plugin

---
+++ pages/list.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="list">
+    <logo />
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue';
+
+export default {
+  components: {
+    Logo,
+  },
+  created() {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,16 +1,18 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASE_URL,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_APP_ID,
+  apiKey: 'AIzaSyAOJzYfk0IYdG_2yGSq1LpCOLXoWRHmg9Q',
+  authDomain: 'kabuker.firebaseapp.com',
+  databaseURL: 'https://kabuker.firebaseio.com',
+  projectId: 'kabuker',
+  storageBucket: 'kabuker.appspot.com',
+  messagingSenderId: '641024804633',
+  appId: '1:641024804633:web:62ee9e2614caef50822788',
 };
-if (!firebase.apps.length) {
-  firebase.initializeApp(config);
-}
 
-export default firebase;
+export default (context, inject) => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  }
+  inject('firebase', firebase);
+};
