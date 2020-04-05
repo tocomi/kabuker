35a1830b3c206c3771925a323cd5be9357f7e7f8
tocomi <tocomi@wish.ocn.ne.jp>
2020-04-05 16:08:10 +0900

    chore: add firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAOJzYfk0IYdG_2yGSq1LpCOLXoWRHmg9Q',
+  authDomain: 'kabuker.firebaseapp.com',
+  databaseURL: 'https://kabuker.firebaseio.com',
+  projectId: 'kabuker',
+  storageBucket: 'kabuker.appspot.com',
+  messagingSenderId: '641024804633',
+  appId: '1:641024804633:web:62ee9e2614caef50822788',
+};
+
+export default firebaseConfig;
