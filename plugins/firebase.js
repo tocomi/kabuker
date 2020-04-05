398d329be2a7fb34e5dca0cbace82ed2bc049e17
tocomi <tocomi@wish.ocn.ne.jp>
2020-04-05 16:21:18 +0900

    chore: add firebase plugin

---
+++ plugins/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+export default firebase;
